fc5cf5fa8714f23805ad824c81775713c8dc576d
Nachox12123 <ignacio.pino.g@ug.uchile.cl>
2026-05-28 21:36:13 -0400

    mapa con sus assets, camara sigue al jugador

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
 
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
+@onready var camera: Camera2D = $Camera2D
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
@@ -27,12 +27,13 @@ func _physics_process(delta: float) -> void:
 	# move_and_slide debe estar fuera del if de autoridad para que
 	# se sincronice correctamente en todos los clientes
 	move_and_slide()
-	#send_position.rpc(global_position)
 	
 func setup(data: Statics.PlayerData) -> void:
 	name = str(data.id)
 	set_multiplayer_authority(data.id, false)
 	multiplayer_synchronizer.set_multiplayer_authority(data.id, false)
+	# Activar la cámara solo para el jugador local (debe ser después de set_multiplayer_authority)
+	camera.enabled = is_multiplayer_authority()
 
 @rpc("authority", "call_remote", "unreliable_ordered")
 func send_position(pos: Vector2) -> void:
